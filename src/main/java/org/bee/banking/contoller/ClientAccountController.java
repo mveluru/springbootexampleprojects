52cbb3d8e1958c875c092241007a91387cf18c66
mveluru <mveluru@gmail.com>
2026-09-10 22:11:14 -0500

    Find All Method Refactored.

--- src/main/java/org/bee/banking/contoller/ClientAccountController.java
+++ src/main/java/org/bee/banking/contoller/ClientAccountController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/accounts")
+@RequestMapping("/v1/api/accounts")
 @RequiredArgsConstructor
 public class ClientAccountController {
     private final ClientAccountService accountService;

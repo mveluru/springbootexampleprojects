cdd08b0555d2a0b0178afcadc406094d2176578b
mveluru <mveluru@gmail.com>
2026-09-17 10:10:44 -0500

    Find All Method Refactored.

--- src/main/java/org/bee/orders/SpringBootProjectsApplication.java
+++ src/main/java/org/bee/orders/SpringBootProjectsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication(scanBasePackages = {"org.bee.orders", "org.bee.product", "org.bee.banking"})
+@SpringBootApplication(scanBasePackages = {"org.bee.orders", "org.bee.product", "org.bee.banking","org.bee.events"})
 @ConfigurationPropertiesScan
 public class SpringBootProjectsApplication {
 

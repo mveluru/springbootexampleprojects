1d8eaebbaefccae97979e0f53edaf20184d39835
mveluru <mveluru@gmail.com>
2026-09-17 11:25:30 -0500

    Find All Method Refactored.

--- src/main/java/org/bee/orders/SpringBootProjectsApplication.java
+++ src/main/java/org/bee/orders/SpringBootProjectsApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication(scanBasePackages = {"org.bee.orders", "org.bee.product", "org.bee.banking","org.bee.events"})
+@SpringBootApplication(scanBasePackages = {"org.bee.orders", "org.bee.product", "org.bee.banking", "org.bee.events", "org.bee.restapi"})
 @EnableAsync
 @ConfigurationPropertiesScan
 @EnableJpaRepositories(basePackages = {"org.bee.orders", "org.bee.product", "org.bee.banking", "org.bee.events"})

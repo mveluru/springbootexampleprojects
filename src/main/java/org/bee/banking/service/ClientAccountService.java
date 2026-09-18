443ab9fa1d39ab57dbe9cfcbcb3b4d0c68eb291e
mveluru <mveluru@gmail.com>
2026-09-17 22:13:22 -0500

    Find All Method Refactored.

--- src/main/java/org/bee/banking/service/ClientAccountService.java
+++ src/main/java/org/bee/banking/service/ClientAccountService.java
@@ -3,17 +3,22 @@ package org.bee.banking.service;
 import lombok.RequiredArgsConstructor;
 import org.bee.banking.component.AccountMapper;
 import org.bee.banking.domain.Account;
+import org.bee.banking.domain.AccountType;
+import org.bee.banking.repository.WithdrawalRespository;
 import org.bee.banking.request.AccountLookupRequest;
 import org.bee.banking.repository.AccountRepository;
 import org.bee.banking.request.AccountRegistrationRequest;
+import org.bee.banking.request.WithdrawalRequest;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class ClientAccountService {
     private final AccountRepository accountRepository;
     private final AccountMapper accountMapper;
+    private final WithdrawalRespository withdrawalRespository;
 
     /**
      * Flow A: Look up consumer account details
@@ -31,5 +36,20 @@ public class ClientAccountService {
 
         // Commits layout back into our static map structure
         return accountRepository.save(newAccountEntity);
+    }
+
+    public void WithdrawAndSaveToAccount(WithdrawalRequest withdrawalRequest) {
+        AccountType requestedAcctType = null;
+        BigDecimal withDrawAmount = withdrawalRequest.getWithdrawAmount();
+        String accountNumber = withdrawalRequest.getAccountNumber();
+        if (accountNumber.substring(0,2).equalsIgnoreCase("CH")){
+           requestedAcctType = AccountType.valueOf("Checking Account");
+        }else if  (accountNumber.substring(0,2).equalsIgnoreCase("SV")){
+             requestedAcctType = AccountType.valueOf("Saving Account");
+        }
+        assert requestedAcctType != null;
+        if (requestedAcctType.equals(withdrawalRequest.getAccountType())){
+            Optional<Account> existingAccount = accountRepository.findByAccountNumber(accountNumber);
+        }
     }
 }

7d26c0f4f20d28a4dbe4d40ee4dfc9c8e4415f8f
mveluru <mveluru@gmail.com>
2026-09-19 13:43:42 -0500

    Refactored the messages

---
+++ src/main/java/org/bee/banking/domain/AccountStatus.java
@@ -0,0 +1,11 @@
+package org.bee.banking.domain;
+
+import java.util.Date;
+
+public class AccountStatus {
+    String accountNumber;
+    String accountType;
+    String accountStatus;
+    Date createdDate;
+    Date closedDate;
+}

f1cb6bba630deb5987c2f67442eef5e11d296103
mveluru <mveluru@gmail.com>
2026-09-19 18:57:41 -0500

    Added new constraints

--- src/main/java/org/bee/banking/domain/AccountStatus.java
+++ src/main/java/org/bee/banking/domain/AccountStatus.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class AccountStatus {
     String accountNumber;
     String accountType;
-    String accountStatus;
-    Date createdDate;
-    Date closedDate;
+    String accountStatus;// ACTIVE , CLOSED
+    Date createdDate;// Not EMPTY
+    Date closedDate; // iF ACTIVE closedDATE will be empty
 }
